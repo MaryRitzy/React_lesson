5bd16b4eca06412754d784d6c29a1bdd3aa251b4
MaryRitzy <atamasM99@gmail.com>
2023-01-31 16:11:18 +0100

    creat one more page Title

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Content } from './Content'
+import Title from './Title'
 
 //jsx  Title > h1
 //Content => div > p * 2
-
-interface ITitleProps {
-    text: string
-}
-
-const Title = (props: ITitleProps) => {
-    console.log(props)
-    return <h1>Hello {props.text}</h1>
-}
 
 function App() {
     return (
