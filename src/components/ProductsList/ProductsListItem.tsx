c0fc0c1aa8125fb3ccebd3890a8e7a9334e4c759
MaryRitzy <atamasM99@gmail.com>
2023-02-16 11:51:11 +0100

    rewrite check Decrement through desiabled

--- src/components/ProductsList/ProductsListItem.tsx
+++ src/components/ProductsList/ProductsListItem.tsx
@@ -31,7 +31,7 @@ const ProductsListItem = ({
         setCount(count + 1)
     }
     const onDecrement = () => {
-        setCount(count <= 0 ? count : count - 1)
+        setCount(count - 1)
     }
 
     return (
@@ -46,11 +46,19 @@ const ProductsListItem = ({
                 <div className="product-features">Capacity: {capacity} Gb</div>
                 <div className="product-price">{price} $</div>
                 <div className="product-quantity">
-                    <Button variant="outlined" onClick={() => onDecrement()}>
+                    <Button
+                        variant="outlined"
+                        onClick={() => onDecrement()}
+                        disabled={count <= 1}
+                    >
                         -
                     </Button>
                     <TextField size="small" value={count} />
-                    <Button variant="outlined" onClick={() => onIncrement()}>
+                    <Button
+                        variant="outlined"
+                        onClick={() => onIncrement()}
+                        disabled={count >= 10}
+                    >
                         +
                     </Button>
                 </div>
