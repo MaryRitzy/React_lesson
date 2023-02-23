a1704dc6c64135ff5b95dc748520112cf375fd27
MaryRitzy <atamasM99@gmail.com>
2023-02-23 15:13:22 +0100

    create separate field for count common sum

--- src/pages/Cart/CartPage.tsx
+++ src/pages/Cart/CartPage.tsx
@@ -1,5 +1,28 @@
-type Props = {}
-const CartPage = (props: Props) => {
-    return <div>CartPage</div>
+import productsArray, { getProductsObject, Product } from 'utils/productsArray'
+
+type Props = {
+    productsInCart: {
+        [id: number]: number
+    }
+    productObject?: {
+        [id: number]: Product
+    }
+}
+
+const CartPage = ({
+    productsInCart,
+    productObject = getProductsObject(productsArray),
+}: Props) => {
+    return (
+        <div>
+            {Object.keys(productsInCart).map((productId) => (
+                <div key={productId}>
+                    {productObject[parseInt(productId)].title} :{' '}
+                    {productsInCart[parseInt(productId)]} :{' '}
+                    {productObject[parseInt(productId)].price}
+                </div>
+            ))}
+        </div>
+    )
 }
 export default CartPage
